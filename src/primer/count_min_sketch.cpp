d600a75bd8ea35fc48ab1ce30a123dc444ea0b84
Van Phan Mac Air <pvphat25@gmail.com>
2025-09-23 22:07:52 -0400

    temp implementation cpp

--- src/primer/count_min_sketch.cpp
+++ src/primer/count_min_sketch.cpp
@@ -1,84 +1,57 @@
-//===----------------------------------------------------------------------===//
-//
-//                         BusTub
-//
-// count_min_sketch.cpp
-//
-// Identification: src/primer/count_min_sketch.cpp
-//
-// Copyright (c) 2015-2025, Carnegie Mellon University Database Group
-//
-//===----------------------------------------------------------------------===//
-
 #include "primer/count_min_sketch.h"
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
 
-#include <stdexcept>
-#include <string>
+namespace bustub{
 
-namespace bustub {
-
-/**
- * Constructor for the count-min sketch.
- *
- * @param width The width of the sketch matrix.
- * @param depth The depth of the sketch matrix.
- * @throws std::invalid_argument if width or depth are zero.
- */
 template <typename KeyType>
 CountMinSketch<KeyType>::CountMinSketch(uint32_t width, uint32_t depth) : width_(width), depth_(depth) {
-  /** @TODO(student) Implement this function! */
+    array_ = new uint32_t*[depth];
+    for (size_t i = 0; i< depth; i+=1) {
+        array_[i] = new uint32_t[width];
+    }
 
-  /** @fall2025 PLEASE DO NOT MODIFY THE FOLLOWING */
-  // Initialize seeded hash functions
-  hash_functions_.reserve(depth_);
-  for (size_t i = 0; i < depth_; i++) {
-    hash_functions_.push_back(this->HashFunction(i));
-  }
+    hash_functions_.reserve(depth_);
+    for (size_t i =0; i < depth_; i+=1) {
+        hash_functions_.push_back(this->HashFunction(i));
+    }
 }
 
 template <typename KeyType>
 CountMinSketch<KeyType>::CountMinSketch(CountMinSketch &&other) noexcept : width_(other.width_), depth_(other.depth_) {
-  /** @TODO(student) Implement this function! */
+    array_ = other.array_;
+    hash_functions_ = other.hash_functions_;
+
+    other.array_ = nullptr;
+    other.hash_functions_ = nullptr;
 }
 
 template <typename KeyType>
 auto CountMinSketch<KeyType>::operator=(CountMinSketch &&other) noexcept -> CountMinSketch & {
-  /** @TODO(student) Implement this function! */
-  return *this;
+    if (this != &other) {
+        delete array_;
+        array_ = other.array_;
+        other.array_ = nullptr;
+    }
+    return *this;
 }
 
 template <typename KeyType>
 void CountMinSketch<KeyType>::Insert(const KeyType &item) {
-  /** @TODO(student) Implement this function! */
-}
-
-template <typename KeyType>
-void CountMinSketch<KeyType>::Merge(const CountMinSketch<KeyType> &other) {
-  if (width_ != other.width_ || depth_ != other.depth_) {
-    throw std::invalid_argument("Incompatible CountMinSketch dimensions for merge.");
-  }
-  /** @TODO(student) Implement this function! */
+    for (size_t row = 0; row < depth_; row += 1) {
+        size_t col = hash_functions_[row](item);
+        array_[row][col] += 1;
+    }
 }
 
 template <typename KeyType>
 auto CountMinSketch<KeyType>::Count(const KeyType &item) const -> uint32_t {
-  return 0;
+    uint32_t res = array_[0][hash_functions_[0](item)];
+    for (int row = 1; row < depth_; row+=1) {
+        res = std::min(res, hash_functions_[row][hash_functions_[row]](item));
+    }
+    return res;
 }
 
-template <typename KeyType>
-void CountMinSketch<KeyType>::Clear() {
-  /** @TODO(student) Implement this function! */
-}
-
-template <typename KeyType>
-auto CountMinSketch<KeyType>::TopK(uint16_t k, const std::vector<KeyType> &candidates)
-    -> std::vector<std::pair<KeyType, uint32_t>> {
-  /** @TODO(student) Implement this function! */
-  return {};
-}
-
-// Explicit instantiations for all types used in tests
-template class CountMinSketch<std::string>;
-template class CountMinSketch<int64_t>;  // For int64_t tests
-template class CountMinSketch<int>;      // This covers both int and int32_t
-}  // namespace bustub
+};
