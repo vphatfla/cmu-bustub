74da623584111c8d76814417cdf7a4ebd73bd22f
Van Phan Mac Air <pvphat25@gmail.com>
2025-09-28 12:41:42 -0400

    use latch for row level

--- src/include/primer/count_min_sketch.h
+++ src/include/primer/count_min_sketch.h
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <functional>
+#include <memory>
 #include <mutex>
 #include <utility>
 #include <vector>
@@ -12,47 +13,47 @@ namespace bustub {
 
 template <typename KeyType>
 class CountMinSketch {
-public:
-    explicit CountMinSketch(uint32_t width, uint32_t depth);
+ public:
+  explicit CountMinSketch(uint32_t width, uint32_t depth);
 
-    CountMinSketch() = delete; // default constructor
-    CountMinSketch(const CountMinSketch &) = delete;
-    auto operator=(const CountMinSketch &) -> CountMinSketch & = delete;
+  CountMinSketch() = delete;  // default constructor
+  CountMinSketch(const CountMinSketch &) = delete;
+  auto operator=(const CountMinSketch &) -> CountMinSketch & = delete;
 
-    CountMinSketch(CountMinSketch &&other) noexcept; // move constructor
-    auto operator=(CountMinSketch &&other) noexcept -> CountMinSketch &; // move assignment
+  CountMinSketch(CountMinSketch &&other) noexcept;                      // move constructor
+  auto operator=(CountMinSketch &&other) noexcept -> CountMinSketch &;  // move assignment
 
-    void Insert(const KeyType &item);
+  void Insert(const KeyType &item);
 
-    auto Count(const KeyType &item) const -> uint32_t;
+  auto Count(const KeyType &item) const -> uint32_t;
 
-    void Clear();
+  void Clear();
 
-    // @brief creates a new sketch by comining counter values from two compatible sketches
-    void Merge(const CountMinSketch<KeyType> &other);
+  // @brief creates a new sketch by comining counter values from two compatible sketches
+  void Merge(const CountMinSketch<KeyType> &other);
 
-    /**
-        @brief Gets the top k items based on estimated counts from a list of candidates
-     **/
-    auto TopK(uint16_t k, const std::vector<KeyType> &candidates) -> std::vector<std::pair<KeyType, uint32_t>>;
+  /**
+      @brief Gets the top k items based on estimated counts from a list of candidates
+   **/
+  auto TopK(uint16_t k, const std::vector<KeyType> &candidates) -> std::vector<std::pair<KeyType, uint32_t>>;
 
-private:
-    uint32_t width_;
-    uint32_t depth_;
-    uint32_t **array_;
-    std::mutex mutex_;
+ private:
+  uint32_t width_;
+  uint32_t depth_;
+  uint32_t **array_;
+  std::vector<std::unique_ptr<std::mutex>> row_mutexes_;
 
-    std::vector<std::function<size_t(const KeyType &)>> hash_functions_;
+  std::vector<std::function<size_t(const KeyType &)>> hash_functions_;
 
-    /** @fall2025 PLEASE DO NOT MODIFY THE FOLLOWING */
-    constexpr static size_t SEED_BASE = 15445;
+  /** @fall2025 PLEASE DO NOT MODIFY THE FOLLOWING */
+  constexpr static size_t SEED_BASE = 15445;
 
-    inline auto HashFunction(size_t seed) -> std::function<size_t(const KeyType &)> {
-        return [seed, this](const KeyType &item) -> size_t {
-            auto h1 = std::hash<KeyType>{}(item);
-            auto h2 = bustub::HashUtil::CombineHashes(seed, SEED_BASE);
-            return bustub::HashUtil::CombineHashes(h1, h2) % width_;
-        };
-    }
+  inline auto HashFunction(size_t seed) -> std::function<size_t(const KeyType &)> {
+    return [seed, this](const KeyType &item) -> size_t {
+      auto h1 = std::hash<KeyType>{}(item);
+      auto h2 = bustub::HashUtil::CombineHashes(seed, SEED_BASE);
+      return bustub::HashUtil::CombineHashes(h1, h2) % width_;
+    };
+  }
 };
-}
+}  // namespace bustub
--- src/primer/count_min_sketch.cpp
+++ src/primer/count_min_sketch.cpp
@@ -3,127 +3,131 @@
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
+#include <memory>
 #include <mutex>
 #include <queue>
 #include <stdexcept>
 #include <utility>
 #include <vector>
 
-namespace bustub{
+namespace bustub {
 
 template <typename KeyType>
 CountMinSketch<KeyType>::CountMinSketch(uint32_t width, uint32_t depth) : width_(width), depth_(depth) {
-    if (width <= 0 || depth <= 0) {
-        throw std::invalid_argument("invalid width or depth, must be positive");
+  if (width <= 0 || depth <= 0) {
+    throw std::invalid_argument("invalid width or depth, must be positive");
+  }
+  array_ = new uint32_t *[depth];
+  for (size_t i = 0; i < depth; i += 1) {
+    array_[i] = new uint32_t[width];
+    for (size_t j = 0; j < width; j += 1) {
+      array_[i][j] = 0;
     }
-    array_ = new uint32_t*[depth];
-    for (size_t i = 0; i< depth; i+=1) {
-        array_[i] = new uint32_t[width];
-        for (size_t j = 0; j < width; j +=1) {
-            array_[i][j] = 0;
-        }
-    }
+  }
 
-    hash_functions_.reserve(depth_);
-    for (size_t i =0; i < depth_; i+=1) {
-        hash_functions_.push_back(this->HashFunction(i));
-    }
+  row_mutexes_.reserve(depth);
+  hash_functions_.reserve(depth_);
+  for (size_t i = 0; i < depth_; i += 1) {
+    row_mutexes_.emplace_back(std::make_unique<std::mutex>());
+    hash_functions_.push_back(this->HashFunction(i));
+  }
 }
 
 template <typename KeyType>
 CountMinSketch<KeyType>::CountMinSketch(CountMinSketch &&other) noexcept : width_(other.width_), depth_(other.depth_) {
-    array_ = other.array_;
-    hash_functions_ = other.hash_functions_;
+  array_ = other.array_;
+  hash_functions_ = std::move(other.hash_functions_);
+  row_mutexes_ = std::move(other.row_mutexes_);
 
-    other.array_ = nullptr;
-    other.hash_functions_.clear();
+  other.array_ = nullptr;
+  other.hash_functions_.clear();
 }
 
 template <typename KeyType>
 auto CountMinSketch<KeyType>::operator=(CountMinSketch &&other) noexcept -> CountMinSketch & {
-    if (this != &other) {
-        for (size_t i = 0; i < depth_; i+=1) {
-            delete[] array_[i];
-        }
-        delete array_;
-
-        width_ = other.width_;
-        depth_ = other.depth_;
-        array_ = other.array_;
-        hash_functions_ = std::move(other.hash_functions_);
-        other.hash_functions_.clear();
+  if (this != &other) {
+    for (size_t i = 0; i < depth_; i += 1) {
+      delete[] array_[i];
     }
-    return *this;
+    delete array_;
+
+    width_ = other.width_;
+    depth_ = other.depth_;
+    array_ = other.array_;
+    hash_functions_ = std::move(other.hash_functions_);
+    row_mutexes_ = std::move(other.row_mutexes_);
+    other.hash_functions_.clear();
+  }
+  return *this;
 }
 
 template <typename KeyType>
 void CountMinSketch<KeyType>::Insert(const KeyType &item) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    for (size_t row = 0; row < depth_; row += 1) {
-        size_t col = hash_functions_[row](item);
-        array_[row][col] += 1;
-    }
+  for (size_t row = 0; row < depth_; row += 1) {
+    std::lock_guard<std::mutex> lock(*row_mutexes_[row]);
+    size_t col = hash_functions_[row](item);
+    array_[row][col] += 1;
+  }
 }
 
 template <typename KeyType>
 auto CountMinSketch<KeyType>::Count(const KeyType &item) const -> uint32_t {
-    uint32_t res = array_[0][hash_functions_[0](item)];
-    for (size_t row = 1; row < depth_; row+=1) {
-        auto col = hash_functions_[row](item);
-        res = std::min(res, array_[row][col]);
-    }
-    return res;
+  uint32_t res = array_[0][hash_functions_[0](item)];
+  for (size_t row = 1; row < depth_; row += 1) {
+    auto col = hash_functions_[row](item);
+    res = std::min(res, array_[row][col]);
+  }
+  return res;
 }
 
 template <typename KeyType>
 void CountMinSketch<KeyType>::Clear() {
-    for (size_t r = 0; r < depth_; r +=1) {
-        for (size_t c = 0; c < width_; c += 1) {
-            array_[r][c] = 0;
-        }
+  for (size_t r = 0; r < depth_; r += 1) {
+    for (size_t c = 0; c < width_; c += 1) {
+      array_[r][c] = 0;
     }
+  }
 };
 
-template<typename KeyType>
+template <typename KeyType>
 void CountMinSketch<KeyType>::Merge(const CountMinSketch<KeyType> &other) {
-    assert(depth_ == other.depth_);
-    assert(width_ == other.width_);
+  assert(depth_ == other.depth_);
+  assert(width_ == other.width_);
 
-    for (size_t r = 0; r < depth_; r +=1) {
-        for (size_t c = 0; c < width_; c += 1) {
-            array_[r][c] += other.array_[r][c];
-        }
+  for (size_t r = 0; r < depth_; r += 1) {
+    for (size_t c = 0; c < width_; c += 1) {
+      array_[r][c] += other.array_[r][c];
     }
+  }
 }
 
-template<typename KeyType>
-auto CountMinSketch<KeyType>::TopK(uint16_t k, const std::vector<KeyType> &candidates) -> std::vector<std::pair<KeyType, uint32_t>> {
-    using PairType = std::pair<KeyType, uint32_t>;
-    auto comp = [](const PairType& a, const PairType& b) {
-      return a.second > b.second;
-     };
-    std::priority_queue<PairType, std::vector<PairType>, decltype(comp)> minHeap(comp);
+template <typename KeyType>
+auto CountMinSketch<KeyType>::TopK(uint16_t k, const std::vector<KeyType> &candidates)
+    -> std::vector<std::pair<KeyType, uint32_t>> {
+  using PairType = std::pair<KeyType, uint32_t>;
+  auto comp = [](const PairType &a, const PairType &b) { return a.second > b.second; };
+  std::priority_queue<PairType, std::vector<PairType>, decltype(comp)> minHeap(comp);
 
-    std::vector<PairType> res;
+  std::vector<PairType> res;
 
-    for (KeyType c: candidates) {
-        uint32_t count = Count(c);
-        minHeap.push({c, count});
-        if (minHeap.size() > k) {
-            minHeap.pop();
-        }
+  for (KeyType c : candidates) {
+    uint32_t count = Count(c);
+    minHeap.push({c, count});
+    if (minHeap.size() > k) {
+      minHeap.pop();
     }
+  }
 
-    while (!minHeap.empty()) {
-        res.push_back(minHeap.top());
-        minHeap.pop();
-    }
+  while (!minHeap.empty()) {
+    res.push_back(minHeap.top());
+    minHeap.pop();
+  }
 
-    std::reverse(res.begin(), res.end());
-    return res;
+  std::reverse(res.begin(), res.end());
+  return res;
 }
 
 template class CountMinSketch<std::string>;
 template class CountMinSketch<int64_t>;  // For int64_t tests
 template class CountMinSketch<int>;      // This covers both int and int32_t
-}
+}  // namespace bustub
