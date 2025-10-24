8b6eb91368273294f46ccf85b5bcfe5a0c40392b
vphatfla <pvphat25@gmail.com>
2025-10-24 14:26:27 -0400

    remove comments

--- src/buffer/arc_replacer.cpp
+++ src/buffer/arc_replacer.cpp
@@ -23,13 +23,6 @@
 
 namespace bustub {
 
-/**
- *
- * TODO(P1): Add implementation
- *
- * @brief a new ArcReplacer, with lists initialized to be empty and target size to 0
- * @param num_frames the maximum number of frames the ArcReplacer will be required to cache
- */
 ArcReplacer::ArcReplacer(size_t num_frames) : replacer_size_(num_frames) { mru_target_size_ = 0; }
 
 auto ArcReplacer::evictFromList(std::list<frame_id_t> &list, std::list<page_id_t> &ghost_list, ArcStatus new_arc_status)
@@ -53,24 +46,7 @@ auto ArcReplacer::evictFromList(std::list<frame_id_t> &list, std::list<page_id_t
   }
   return std::nullopt;
 }
-/**
- * TODO(P1): Add implementation
- *
- * @brief Performs the Replace operation as described by the writeup
- * that evicts from either mfu_ or mru_ into its corresponding ghost list
- * according to balancing policy.
- *
- * If you wish to refer to the original ARC paper, please note that there are
- * two changes in our implementation:
- * 1. When the size of mru_ equals the target size, we don't check
- * the last access as the paper did when deciding which list to evict from.
- * This is fine since the original decision is stated to be arbitrary.
- * 2. Entries that are not evictable are skipped. If all entries from the desired side
- * (mru_ / mfu_) are pinned, we instead try victimize the other side (mfu_ / mru_),
- * and move it to its corresponding ghost list (mfu_ghost_ / mru_ghost_).
- *
- * @return frame id of the evicted frame, or std::nullopt if cannot evict
- */
+
 auto ArcReplacer::Evict() -> std::optional<frame_id_t> {
   // If mru_.size < target, evict from mfu_, if can't evict from mru_
   // else, evict from mru_, if can't evict from mfu_
@@ -93,35 +69,6 @@ auto ArcReplacer::Evict() -> std::optional<frame_id_t> {
   return res;
 }
 
-/**
- * TODO(P1): Add implementation
- *
- * @brief Record access to a frame, adjusting ARC bookkeeping accordingly
- * by bring the accessed page to the front of mfu_ if it exists in any of the lists
- * or the front of mru_ if it does not.
- *
- * Performs the operations EXCEPT REPLACE described in original paper, which is
- * handled by `Evict()`.
- *
- * Consider the following four cases, handle accordingly:
- * 1. Access hits mru_ or mfu_
- * 2/3. Access hits mru_ghost_ / mfu_ghost_
- * 4. Access misses all the lists
- *
- * This routine performs all changes to the four lists as preperation
- * for `Evict()` to simply find and evict a victim into ghost lists.
- *
- * Note that frame_id is used as identifier for alive pages and
- * page_id is used as identifier for the ghost pages, since page_id is
- * the unique identifier to the page after it's dead.
- * Using page_id for alive pages should be the same since it's one to one mapping,
- * but using frame_id is slightly more intuitive.
- *
- * @param frame_id id of frame that received a new access.
- * @param page_id id of page that is mapped to the frame.
- * @param access_type type of access that was received. This parameter is only needed for
- * leaderboard tests.
- */
 void ArcReplacer::RecordAccess(frame_id_t frame_id, page_id_t page_id, [[maybe_unused]] AccessType access_type) {
   // 1. cache HIT: if page exits in MRU OR MFU
   // move page to front of MFU
@@ -227,23 +174,6 @@ void ArcReplacer::RecordAccess(frame_id_t frame_id, page_id_t page_id, [[maybe_u
   curr_size_ += 1;
 }
 
-/**
- * TODO(P1): Add implementation
- *
- * @brief Toggle whether a frame is evictable or non-evictable. This function also
- * controls replacer's size. Note that size is equal to number of evictable entries.
- *
- * If a frame was previously evictable and is to be set to non-evictable, then size should
- * decrement. If a frame was previously non-evictable and is to be set to evictable,
- * then size should increment.
- *
- * If frame id is invalid, throw an exception or abort the process.
- *
- * For other scenarios, this function should terminate without modifying anything.
- *
- * @param frame_id id of frame whose 'evictable' status will be modified
- * @param set_evictable whether the given frame is evictable or not
- */
 void ArcReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
   auto it = alive_map_.find(frame_id);
   if (it == alive_map_.end()) {
@@ -258,22 +188,6 @@ void ArcReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
   f_status->evictable_ = set_evictable;
 }
 
-/**
- * TODO(P1): Add implementation
- *
- * @brief Remove an evictable frame from replacer.
- * This function should also decrement replacer's size if removal is successful.
- *
- * Note that this is different from evicting a frame, which always remove the frame
- * decided by the ARC algorithm.
- *
- * If Remove is called on a non-evictable frame, throw an exception or abort the
- * process.
- *
- * If specified frame is not found, directly return from this function.
- *
- * @param frame_id id of frame to be removed
- */
 void ArcReplacer::Remove(frame_id_t frame_id) {
   if (auto it = alive_map_.find(frame_id); it != alive_map_.end()) {
     auto f_status = it->second;
@@ -293,13 +207,6 @@ void ArcReplacer::Remove(frame_id_t frame_id) {
   }
 }
 
-/**
- * TODO(P1): Add implementation
- *
- * @brief Return replacer's size, which tracks the number of evictable frames.
- *
- * @return size_t
- */
 auto ArcReplacer::Size() -> size_t { return curr_size_; }
 
 }  // namespace bustub
