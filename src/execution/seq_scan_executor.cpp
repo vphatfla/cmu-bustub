eadd3189da7a462ab7a7cbeec5a916ab9679bac1
vphatfla <pvphat25@gmail.com>
2026-06-20 21:54:26 -0400

    seq scan

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,6 +11,10 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include <cstddef>
+#include <memory>
+#include <optional>
+#include <utility>
 #include "common/macros.h"
 
 namespace bustub {
@@ -20,12 +24,14 @@ namespace bustub {
  * @param exec_ctx The executor context
  * @param plan The sequential scan plan to be executed
  */
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {
-  UNIMPLEMENTED("TODO(P3): Add implementation.");
-}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
+    : AbstractExecutor(exec_ctx), plan_(plan) {}
 
 /** Initialize the sequential scan */
-void SeqScanExecutor::Init() { UNIMPLEMENTED("TODO(P3): Add implementation."); }
+void SeqScanExecutor::Init() {
+  table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->table_name_);
+  table_iterator_.emplace(table_info_->table_->MakeIterator());
+}
 
 /**
  * Yield the next tuple batch from the seq scan.
@@ -36,7 +42,33 @@ void SeqScanExecutor::Init() { UNIMPLEMENTED("TODO(P3): Add implementation."); }
  */
 auto SeqScanExecutor::Next(std::vector<bustub::Tuple> *tuple_batch, std::vector<bustub::RID> *rid_batch,
                            size_t batch_size) -> bool {
-  UNIMPLEMENTED("TODO(P3): Add implementation.");
+  tuple_batch->clear();
+  rid_batch->clear();
+
+  tuple_batch->reserve(batch_size);
+  rid_batch->reserve(batch_size);
+
+  while (!table_iterator_->IsEnd() && batch_size > 0) {
+    auto [tuplemeta, tuple] = table_iterator_->GetTuple();
+    if (tuplemeta.is_deleted_) {
+      ++table_iterator_.value();
+      continue;
+    }
+    if (plan_->filter_predicate_ != nullptr) {
+      auto result = plan_->filter_predicate_->Evaluate(&tuple, table_info_->schema_);
+      if (result.IsNull() || !result.GetAs<bool>()) {
+        ++table_iterator_.value();
+        continue;
+      }
+    }
+
+    rid_batch->emplace_back(tuple.GetRid());
+    tuple_batch->emplace_back(std::move(tuple));
+    batch_size--;
+    ++table_iterator_.value();
+  }
+
+  return !tuple_batch->empty();
 }
 
 }  // namespace bustub
--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -12,11 +12,15 @@
 
 #pragma once
 
+#include <memory>
+#include <optional>
 #include <vector>
 
+#include "catalog/catalog.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/seq_scan_plan.h"
+#include "storage/table/table_iterator.h"
 #include "storage/table/tuple.h"
 
 namespace bustub {
@@ -39,5 +43,9 @@ class SeqScanExecutor : public AbstractExecutor {
  private:
   /** The sequential scan plan node to be executed */
   const SeqScanPlanNode *plan_;
+  /** Table Info, hold table heap and table schema */
+  std::shared_ptr<TableInfo> table_info_{};
+  /** Table Iterator for table table in table_info_ */
+  std::optional<TableIterator> table_iterator_{};
 };
 }  // namespace bustub
